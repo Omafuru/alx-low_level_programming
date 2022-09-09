6779e909ac54355b6a348a97b4c9f8bb725fe1c7
Onome Mafuru <onomemafuru@gmail.com>
2022-09-09 08:48:25 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints out 0 - 9
+ * followed by a new line
+ * Return: Aways 0 (Sucess)
+ */
+
+int main(void)
+{
+	ch = 0;
+
+	while (ch < 10)
+	{
+		printf("%d\n", ch);
+		ch++;
+
+	}
+
+	return (0);
+
+
+}
