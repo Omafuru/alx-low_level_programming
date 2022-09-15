b4d8f0180bbfe687046a8ec52a7c00805ffed079
Onome Mafuru <onomemafuru@gmail.com>
2022-09-15 14:32:03 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i < 10; i++)
+	for (num = 0; num <= 9; ++num)
 	{
-		if (i != 2 && i != 4)
-		{
+		if (num == 2 || num == 4)
+			continue
 
 			_putchar(i + '0');
 		}
