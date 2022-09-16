be100b981db0f631a717ea8e4f155c07eff81a4d
Onome Mafuru <onomemafuru@gmail.com>
2022-09-16 07:19:51 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,26 +8,31 @@
  */
 int main(void)
 {
-	int i;
+	int num = 1;
 
-	for (i = 1; i <= 100; i++)
+	while (num <= 100)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
-			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf(" FizzBuzz");
-		} else if (i == 1)
-		{
-			printf("%d", i);
-		} else
-		{
-			printf(" %d", i);
+			printf("FizzBuzz ");
 		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5)
+		{
+			printf("Buzz ");
+		else
+			{
+				printf("%i", i);
+
+			if (num != 100)
+			{
+				putchar(' ');
+			}
+			i++;
 	}
-	printf("\n");
-	return (0);
+		putchar('\n');
+		return (0);
 }
