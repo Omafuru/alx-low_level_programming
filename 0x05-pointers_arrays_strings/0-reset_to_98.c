7bfde11171c7db7f73eef28a50096fdc33827173
Onome Mafuru <onomemafuru@gmail.com>
2022-09-19 11:25:29 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,6 +4,8 @@
  * main - a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98.
  *
+ * @n: pointer to the variable to update
+ *
  * Return: Always 0.
  */
 
