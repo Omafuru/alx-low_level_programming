573aa972c986b7f1fb94f5566ed81725dfeff9ed
Onome Mafuru <onomemafuru@gmail.com>
2022-09-19 12:01:12 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string to evaluate
+ *
+ * Return : Length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		{
+			i++;
+		}
+	return (i);
+}
