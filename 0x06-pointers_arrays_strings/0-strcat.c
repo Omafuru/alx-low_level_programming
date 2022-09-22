7e0c9c0abd31f094780dfdafffc4b7031dc8842b
Onome Mafuru <onomemafuru@gmail.com>
2022-09-22 15:13:23 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,18 +12,13 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0')
+
+	while (dest[i])
 		i++;
 
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
+	for (j = 0; src[j] ; j++)
+		dest[i++] = src[j];
 
-	dest[i] = '\0';
 	return (dest);
 
 }
