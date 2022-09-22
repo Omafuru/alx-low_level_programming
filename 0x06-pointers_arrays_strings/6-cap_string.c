c7d908175b0475a2432a9dd4a0072afdd06da063
Onome Mafuru <onomemafuru@gmail.com>
2022-09-22 01:57:33 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,32 +6,80 @@
  */
 char *cap_string(char *str)
 {
-	int i, j;
+	char c;
+	int i, start;
+	int capitalize = 0;
 
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
-		'!', '?', '"', '(', ')', '{', '}'};
-
-	for (i = 0; s[i] != '\0'; i++)
+	start = 1;
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		c = str[i];
+		if (start == 1)
 		{
-			s[i] -= 32;
+			start = 0;
+			capitalize = 1;
 		}
-		for (j = 0; j < 13; j++)
+		if (is_delimiter(c) == 1)
 		{
-			if (s[i] == spe[j])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				{
-					s[i + 1] -= 32;
-				}
+			capitalize = 1;
+			continue;
+		}
 
-			}
-
+		if (capitalize == 1)
+		{
+			if (_islower(c) == 1)
+				str[i] = (int)c - 97 + 65;
+			capitalize = 0;
 		}
 
 	}
 
-	return (s);
+	return (str);
 
+}
+
+/**
+ * is_delimiter - Checks if a character is a delimiter
+ * @c: The character to be tested
+ *
+ * Return: 1 if the character is a delimiter, 0 otherwise
+ */
+int is_delimiter(char c)
+{
+	int res, i;
+	char delimiters[] = " \t\n,;.!?\"(){}";
+
+	res = 0;
+	i = 0;
+	for (i = 0; delimiters[i] != '\0'; i++)
+	{
+		if (delimiters[i] == c)
+		{
+			res = 1;
+			break;
+		}
+	}
+	return (res);
+}
+
+/**
+ * _islower - Checks for uppercase
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	int res, lower, max;
+
+	lower = 97;
+	max = lower + 25;
+
+	if (c >= lower && c <= max)
+		res = 1;
+	else
+		res = 0;
+
+	return (res);
 }
